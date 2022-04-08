5ee505db26794c0c5aacc6815b7224209e53b89f
Kirk Morris <worker.kirk@gmail.com>
2022-04-07 19:15:33 -0700

    Initial commit.

---
+++ src/scenes/BootScene.js
@@ -0,0 +1,76 @@
+import {Scene} from 'phaser';
+
+class BootScene extends Scene {
+  constructor() {
+    super("scene-boot");
+  }
+  
+  preload() {
+    // Load any assets here from your assets directory
+    this.load.image('cat-like', 'assets/cat-like-creature.png');
+
+    this.load.atlas('tabby', 'assets/sprites/tabby.png', 'assets/sprites/tabby.json');
+
+    this.load.image('tileset1', 'assets/maps/tileset1-ex.png');
+    this.load.tilemapTiledJSON('map-level1', 'assets/maps/level1.json');
+  }
+
+  create() {
+    // Create animations
+    this.anims.create({
+      key: 'tabby-idle',
+      frames: this.anims.generateFrameNames('tabby', {
+        start: 0,
+        end: 59,
+        prefix: 'idle-',
+        suffix: '.png',
+        zeroPad: 2
+      }),
+      frameRate: 80,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'tabby-run',
+      frames: this.anims.generateFrameNames('tabby', {
+        start: 0,
+        end: 59,
+        prefix: 'run-',
+        suffix: '.png',
+        zeroPad: 2
+      }),
+      frameRate: 80,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'tabby-flip',
+      frames: this.anims.generateFrameNames('tabby', {
+        start: 0,
+        end: 9,
+        prefix: 'flip-',
+        suffix: '.png',
+        zeroPad: 0
+      }),
+      frameRate: 60,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'tabby-fall',
+      frames: this.anims.generateFrameNames('tabby', {
+        start: 0,
+        end: 9,
+        prefix: 'fall-',
+        suffix: '.png',
+        zeroPad: 0
+      }),
+      frameRate: 60,
+      repeat: -1
+    });
+
+    this.scene.start('scene-game');
+  }
+}
+
+export default BootScene;
---
+++ src/scenes/GameScene.js
@@ -0,0 +1,141 @@
+import {Scene} from 'phaser';
+
+class GameScene extends Scene {
+  constructor() {
+    super("scene-game");
+  }
+
+  create() {
+    this.cat = this.physics.add.sprite(0, 0, 'tabby');
+    this.cat.body.setSize(40, 65);
+    this.cat.body.setOffset(5, 0);
+    this.maxSpeed = 275;
+    this.jumpHeight = 450;
+    this.cat.body.setMaxVelocityY(450);
+
+    this.tilemap = this.add.tilemap('map-level1');
+    const tiles = this.tilemap.addTilesetImage('tileset1', 'tileset1', 32, 32, 1, 2);
+
+    this.ground = this.tilemap.createLayer('ground', tiles);
+
+    this.ground.setCollisionByProperty({ collides: true });
+
+    const spawnPoints = this.tilemap.getObjectLayer('spawn').objects;
+
+    spawnPoints.forEach(({x, y, name}) => {
+      if (name === 'tabby') {
+        this.cat.setPosition(x, y);
+      }
+    });
+
+    this.physics.add.collider(this.cat, this.ground);
+
+    this.ground.setDepth(0);
+    this.cat.setDepth(1);
+    
+    // Create a helper object for our arrow keys
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    // Touch controls
+    this.isTouchControlled = false;
+    this.jumpThreshold = 10;
+    this.sx = null;
+    this.sy = null;
+
+    this.input.on('pointerdown', ({x, y}) => {
+      this.sx = x;
+      this.sy = y;
+      this.isTouchControlled = true;
+    });
+
+    this.input.on('pointermove', ({x, y}) => {
+      if (this.sx !== null && this.sy !== null) {
+        const dx = -(this.sx - x);
+        const dy = (this.sy - y);
+
+        const vx = (Math.max(Math.min(dx, this.maxSpeed), -this.maxSpeed) * 2);
+
+        this.cat.setVelocityX(vx);
+
+        if (dx < 0) {
+          this.cat.setFlipX(true);
+        }
+        else {
+          this.cat.setFlipX(false);
+        }
+
+        if (dy > this.jumpThreshold) {
+          this.cat.setVelocityY(-this.jumpHeight);
+        }
+
+        this.sy = y;
+      }
+    });
+
+    this.input.on('pointerup', () => {
+      this.sx = null;
+      this.sy = null;
+      this.cat.setVelocityX(0);
+    });
+
+    this.cameras.main.startFollow(this.cat);
+    this.cameras.main.setBounds(0, 0, this.tilemap.widthInPixels, this.tilemap.heightInPixels);
+    this.cameras.main.setBackgroundColor(0x337788);
+  }
+
+  update() {
+    // Listen for keyboard input
+    const {left, right, up, down} = this.cursors;
+    const isGrounded = this.cat.body.blocked.down;
+
+    if (left.isDown) {
+      this.cat.setVelocityX(-this.maxSpeed);
+      this.cat.setFlipX(true);
+      this.isTouchControlled = false;
+    }
+    else if (right.isDown) {
+      this.cat.setVelocityX(this.maxSpeed);
+      this.cat.setFlipX(false);
+      this.isTouchControlled = false;
+    }
+    else if (!this.isTouchControlled) {
+      this.cat.setVelocityX(0);
+    }
+    
+    if (up.isDown && isGrounded) {
+      this.cat.setVelocityY(-this.jumpHeight);
+      this.isTouchControlled = false;
+    }
+
+    const {x: vx, y: vy} = this.cat.body.velocity;
+
+    if (isGrounded) {
+      this.cat.rotation = 0;
+
+      if (vx === 0) {
+        this.cat.play('tabby-idle', true);
+      }
+      else {
+        this.cat.play('tabby-run', true);
+      }
+    }
+    else {
+      if (vy <= 0) {
+        this.cat.play('tabby-flip', true);
+
+        if (this.cat.flipX) {
+          this.cat.rotation -= 0.15;
+        }
+        else {
+          this.cat.rotation += 0.15;
+        }
+      }
+      else {
+        this.cat.play('tabby-fall', true);
+        this.cat.rotation = 0;
+      }
+    }
+  }
+
+}
+export default GameScene;
